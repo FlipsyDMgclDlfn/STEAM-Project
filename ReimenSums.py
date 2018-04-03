4dab7f80bee04288be86058d0688bd66ca58bc01
FlipsyDMgclDlfn <34141795+FlipsyDMgclDlfn@users.noreply.github.com>
2018-04-03 14:38:51 -0400

    Update ReimenSums.py

--- ReimenSums.py
+++ ReimenSums.py
@@ -1,5 +1,3 @@
-from tkinter import *
-
 class Polynomial(object):
   
   ##the code runs this automatically when initializing a polynomial
@@ -54,6 +52,12 @@ class Polynomial(object):
   def __len__(self):
     return max(self.coeff)
 
+
+
+
+
+
+
 def calculateRight(start, end, step, poly):
   area = 0
   for x in my_range(start+step, end, step):
@@ -63,16 +67,58 @@ def calculateRight(start, end, step, poly):
 def calculateLeft(start, end, step, poly):
   area = 0
   for x in my_range(start, end-step, step):
-    area += poly.evaluate(x)
-  return area*step
+    area += poly.evaluate(x) * step
+  return area
 
 def my_range(start, end, step):
     while start <= end:
         yield start
         start += step
-
-class ReimenSums:
-    def __init__(self):
         
+def getCords(poly,start,end,width):
+  xs = []
+  ys = []
+  for i in my_range(start,end+end/width,end/width):
+    xs += [i]
+    ys += [poly.evaluate(i)]
+    
+  xStep = ((max(xs)-min(xs))/width)
+  yStep = ((max(ys)-min(ys))/width)
+  
+  adjust = 0-(min(ys)/yStep)
+  
+  cords = []
+  for i in range(0,len(xs)):
+    cords += [[round(xs[i]/xStep),round(ys[i]/yStep + adjust)]]
+  return cords
 
-master = Tk()
+def getRectLeft(poly,start,end,step,width):
+  xs = []
+  ys = []
+  for i in my_range(start,end+end/width,end/width):
+    xs += [i]
+    ys += [poly.evaluate(i)]
+  cords = []
+  funcRange = end - start
+  xStep = funcRange / width
+  rectWidth = step / xStep
+  for i in my_range(0,width,rectWidth):
+    cords += [([round(xs[i]),round(ys[i])],[round(xs[i+rectWidth]),round(ys[i+rectWidth])])]
+  return cords
+  
+poly = Polynomial({0:-1,2:1}) ##Polynomial working with
+width = 400                   ##Width of the graph (also the height)
+start = 0                     ##Starting bounds
+end = 2                       ##Ending bounds
+step = .5                     ##Width of rectangles
+print(getRectLeft(poly,start,end,step,width))
+
+
+  
+
+
+
+
+
+
+
