39c3d6bd9966b84127f287328f1dc4db4cc4bd20
FlipsyDMgclDlfn <34141795+FlipsyDMgclDlfn@users.noreply.github.com>
2018-04-06 10:40:13 -0400

    Can now change Polynomial

--- ReimenSums.py
+++ ReimenSums.py
@@ -114,24 +114,69 @@ def getRectRight(poly,start,end,step,width):
 
 class RiemannSums:
   def __init__(self):
-    self.poly = Polynomial({0:0,3:1,2:-1})   ##Polynomial working with
+    self.poly = Polynomial({0:0,2:1})   ##Polynomial working with
     self.width = 400                    ##Width of the graph (also the height)
     self.start = 0                      ##Starting bounds
     self.end = 5                        ##Ending bounds
     self.step = .1                      ##Width of rectangles
     self.C = Canvas(master, bg = "white", height = 600, width = 500)
     self.update()
-    B = Scale(master,from_=0.01,to=1,command=self.updateSlider,orient=HORIZONTAL,resolution=.01)
-    B.set(.5)
-    B.pack()
-    C = Scale(master,from_=1,to=10,command=self.updateSlider2,orient=HORIZONTAL,resolution=1)
-    C.set(2)
-    C.pack()
+    S1 = Scale(master,from_=0.01,to=1,command=self.updateSlider,orient=HORIZONTAL,resolution=.01)
+    S1.set(.5)
+    S1.grid(row=1,columnspan=8)
+    S2 = Scale(master,from_=1,to=10,command=self.updateSlider2,orient=HORIZONTAL,resolution=1)
+    S2.set(2)
+    S2.grid(row=1,column=8,columnspan=8)
+    LA = Label(master,text="a")
+    LA.grid(row=2,column=2)
+    LB = Label(master,text="bx")
+    LB.grid(row=2,column=4)
+    LC = Label(master,text="cx^2")
+    LC.grid(row=2,column=6)
+    LD = Label(master,text="dx^3")
+    LD.grid(row=2,column=8)
+    LE = Label(master,text="ex^4")
+    LE.grid(row=2,column=10)
+    LF = Label(master,text="fx^5")
+    LF.grid(row=2,column=12)
+    L1 = Label(master,text="+")
+    L1.grid(row=2,column=3)
+    L2 = Label(master,text="+")
+    L2.grid(row=2,column=5)
+    L3 = Label(master,text="+")
+    L3.grid(row=2,column=7)
+    L4 = Label(master,text="+")
+    L4.grid(row=2,column=9)
+    L5 = Label(master,text="+")
+    L5.grid(row=2,column=11)
+    EA = Entry(master,width=3)
+    EA.grid(row=3,column=2)
+    EA.insert(0,0)
+    EB = Entry(master,width=3)
+    EB.grid(row=3,column=4)
+    EB.insert(0,0)
+    EC = Entry(master,width=3)
+    EC.grid(row=3,column=6)
+    EC.insert(0,2)
+    ED = Entry(master,width=3)
+    ED.grid(row=3,column=8)
+    ED.insert(0,0)
+    EE = Entry(master,width=3)
+    EE.grid(row=3,column=10)
+    EE.insert(0,0)
+    EF = Entry(master,width=3)
+    EF.grid(row=3,column=12)
+    EF.insert(0,0)
+    B = Button(master,text="update",command=lambda:self.updateButton(EA.get(),EB.get(),EC.get(),ED.get(),EE.get(),EF.get()))
+    B.grid(row=4,column=7)
   def updateSlider(self,step):
     self.step = float(step)
     self.update()
   def updateSlider2(self,end):
     self.end = int(end)
+    self.update()
+  def updateButton(self,a,b,c,d,e,f):
+    self.poly= Polynomial({0:int(a),1:int(b),2:int(c),3:int(d),4:int(e),5:int(f)})
     self.update()
   def update(self):
     self.C.delete(ALL)
@@ -150,7 +195,7 @@ class RiemannSums:
     cords = getCords(self.poly,self.start,self.end,self.width)
     for i in range(0,len(cords)-1):
       lines += [self.C.create_line(50+cords[i][0],50+cords[i][1],50+cords[i+1][0],50+cords[i+1][1],fill="red")]
-      self.C.pack()
+      self.C.grid(row=0,columnspan=16)
     
     rectCords = getRectRight(self.poly,self.start,self.end,self.step,self.width)
     for i in range(0,len(rectCords)):
@@ -169,7 +214,7 @@ class RiemannSums:
     ##self.C.create_text(215,540,text=str(self.start),font=("Courier", 8))
     ##self.C.create_text(215,560,text=str(self.end),font=("Courier", 8))
     
-    self.C.pack()
+    self.C.grid(row=0,columnspan=16)
     
 
 master = Tk()
